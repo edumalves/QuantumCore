d7ad53e87ee3ab523d1414b2ef1eba34cd05f0fe
Eduardo Alves <ealves@me.com>
2022-11-14 00:22:52 -0300

    Initial abstraction between Quantum Ventures and Tao Core credentials and utils libs.

---
+++ src/main/java/com/quantumventures/Credentials.java
@@ -0,0 +1,12 @@
+package com.quantumventures;
+
+/**
+ * Credentials loader for Quantum Ventures.
+ * Based on Azure Key Vault
+ *
+ * @author Eduardo Alves
+ * @version 1.0
+ */
+public class Credentials {
+
+}
--- src/main/java/com/quantumventures/Main.java
+++ src/main/java/com/quantumventures/Main.java
@@ -1,4 +1,4 @@
-package com.quantumfinance;
+package com.quantumventures;
 
 public class Main {
     public static void main(String[] args) {
